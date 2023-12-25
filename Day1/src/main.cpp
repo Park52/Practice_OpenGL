b4ceea0882804d2102c8eb8664e8cc8405be9921
park52 <wogus8660@gmail.com>
2023-12-26 00:58:41 +0900

    [박재현, Day1 Commit] - CMakeList 추가 작성 및 분리     - glfw Library     - SPDLOG Library - Sample Cpp 작성

--- Day1/src/main.cpp
+++ Day1/src/main.cpp
@@ -1,7 +1,34 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv)
 {
-    std::cout << "Hello, world!" << std::endl;
+    SPDLOG_INFO("Start program");
+    SPDLOG_INFO("Initialize glfw");
+
+    if (!glfwInit())
+    {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Practice_OpenGL
+
+cmake -Bbuild . -DCMAKE_BUILD_TYPE=Debug
+cmake --build build --config Debug
