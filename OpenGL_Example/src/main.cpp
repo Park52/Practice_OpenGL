2873b456627262497ab416b5a9d70d98592150ab
park52 <wogus8660@gmail.com>
2024-01-03 01:30:08 +0900

    [박재현, Day3 Commit] - OpenGL_Example 프로젝트 및 설정 - shader Load 클래스 설정 - 스마트 포인터 확인 - shader code compile

---
+++ OpenGL_Example/src/main.cpp
@@ -0,0 +1,96 @@
+﻿#include "common.h"
+#include "shader.h"
+
+#include <spdlog/spdlog.h>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void OnFramebufferSizeChange(GLFWwindow* window, int width, int height)
+{
+    SPDLOG_INFO("framebuffer size changed: ({} x {})", width, height);
+    //  OpenGL�� �׸��� �׸� ȭ���� ��ġ �� ũ�� ����
+    glViewport(0, 0, width, height);
+}
+
+void OnKeyEvent(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    SPDLOG_INFO("key: {}, scancode: {}, action: {}, mods: {}{}{}",
+        key,
+        scancode,
+        action == GLFW_PRESS ? "Pressed" :
+        action == GLFW_RELEASE ? "Released" :
+        action == GLFW_REPEAT ? "Repat" : "Unknown",
+        mods & GLFW_MOD_CONTROL ? "C" : "-",
+        mods & GLFW_MOD_SHIFT ? "S" : "-",
+        mods & GLFW_MOD_ALT ? "A" : "-");
+
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void Render()
+{
+    //  ȭ���� ���� �� ���� ����
+    glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+    //  Frame Buffer�� Clear
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+int main(int argc, const char** argv)
+{
+    SPDLOG_INFO("Start program");
+    SPDLOG_INFO("Initialize glfw");
+
+    if (!glfwInit())
+    {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    SPDLOG_INFO("Create glfw window");
+    auto window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME, nullptr, nullptr);
+    if (!window)
+    {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // glad�� Ȱ���� OpenGL �Լ� �ε� 
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        SPDLOG_ERROR("failed to initialize glad");
+        glfwTerminate();
+        return -1;
+    }
+    auto glVersion = glGetString(GL_VERSION);
+    SPDLOG_INFO("OpenGL context version: {}", (const char*)glVersion);
+
+    auto vertexShader = Shader::CreateFromFile("./shader/simple.vs", GL_VERTEX_SHADER);
+    auto fragmentShader = Shader::CreateFromFile("./shader/simple.fs", GL_FRAGMENT_SHADER);
+    SPDLOG_INFO("vertex shader id: {}", vertexShader->Get());
+    SPDLOG_INFO("fragment shader id: {}", fragmentShader->Get());
+
+    OnFramebufferSizeChange(window, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, OnFramebufferSizeChange);
+    glfwSetKeyCallback(window, OnKeyEvent);
+
+    SPDLOG_INFO("Start main loop");
+    while (!glfwWindowShouldClose(window))
+    {
+        Render();
+        glfwSwapBuffers(window); 
+        glfwPollEvents(); 
+    }
+
+    glfwTerminate();
+    return 0;
+}
