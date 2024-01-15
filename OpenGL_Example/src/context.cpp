9e852814a14f2113d2772cf79ea18c7a4a9e017c
park52 <wogus8660@gmail.com>
2024-01-16 01:04:30 +0900

    [박재현, Day5 Commit] - Texture 기본 설정     - glgenTextures / glBindTextures / glTexImage2D - image 로딩     - stb 사용해서 로딩 - texture vertex shader / texture fragment shader 생성

--- OpenGL_Example/src/context.cpp
+++ OpenGL_Example/src/context.cpp
@@ -1,5 +1,5 @@
 #include "context.h"
-
+#include "image.h"
 ContextUPtr Context::Create()
 {
     auto context = ContextUPtr(new Context());
@@ -11,10 +11,10 @@ ContextUPtr Context::Create()
 bool Context::Init() 
 {
     float vertices[] = {
-        0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f,   // top right, red
-        0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right, green
-        -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, // bottom left, blue
-        -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f,  // top left, yellow
+    0.5f, 0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f,
+    0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f,
+    -0.5f, 0.5f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f,
     };
     uint32_t indices[] = {
         // note that we start from 0!
@@ -23,16 +23,18 @@ bool Context::Init()
     };
 
     m_vertexLayout = VertexLayout::Create();
-    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 24);
+    m_vertexBuffer = Buffer::CreateWithData(GL_ARRAY_BUFFER, GL_STATIC_DRAW, vertices, sizeof(float) * 32);
 
     m_vertexLayout->SetAttrib(0, 3, GL_FLOAT, GL_FALSE,
-                              sizeof(float) * 6, 0);
+                              sizeof(float) * 8, 0);
     m_vertexLayout->SetAttrib(1, 3, GL_FLOAT, GL_FALSE,
-                              sizeof(float) * 6, sizeof(float) * 3);
+                              sizeof(float) * 8, sizeof(float) * 3);
+    m_vertexLayout->SetAttrib(2, 2, GL_FLOAT, GL_FALSE,
+                              sizeof(float) * 8, sizeof(float) * 6);
     m_indexBuffer = Buffer::CreateWithData(GL_ELEMENT_ARRAY_BUFFER, GL_STATIC_DRAW, indices, sizeof(uint32_t)*6);
 
-    ShaderPtr vertexShader = Shader::CreateFromFile("./shader/per_vertex_color.vs", GL_VERTEX_SHADER);
-    ShaderPtr fragmentShader = Shader::CreateFromFile("./shader/per_vertex_color.fs", GL_FRAGMENT_SHADER);
+    ShaderPtr vertexShader = Shader::CreateFromFile("./shader/texture.vs", GL_VERTEX_SHADER);
+    ShaderPtr fragmentShader = Shader::CreateFromFile("./shader/texture.fs", GL_FRAGMENT_SHADER);
     if(!vertexShader || !fragmentShader)
     {
         return false;
@@ -50,6 +52,23 @@ bool Context::Init()
     SPDLOG_INFO("program id: {}", m_program->Get());
 
     glClearColor(0.0f, 0.1f, 0.2f, 0.0f);
+
+    auto image = Image::Load("./image/container.jpg");
+    if (!image)
+        return false;
+    SPDLOG_INFO("image: {}x{}, {} channels",
+                image->GetWidth(), image->GetHeight(), image->GetChannelCount());
+
+    glGenTextures(1, &m_texture);
+    glBindTexture(GL_TEXTURE_2D, m_texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8,
+                 image->GetWidth(), image->GetHeight(), 0,
+                 GL_RGB, GL_UNSIGNED_BYTE, image->GetData());
 
     return true;
 }
