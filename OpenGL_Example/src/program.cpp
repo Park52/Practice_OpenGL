3d701a1af0138553b8a8064bdd13073f182c0071
park52 <wogus8660@gmail.com>
2024-02-04 23:18:03 +0900

    [박재현, Day6 Commit] - Coordinate System, 좌표 공간 간의 변환 - MAP Matrix (Model, View, Projection Matrix) - 직교 투영, 원근 투영 - Depth Buffer, DEPTH_TEST

--- OpenGL_Example/src/program.cpp
+++ OpenGL_Example/src/program.cpp
@@ -1,7 +1,6 @@
 #include "program.h"
 
-ProgramUPtr Program::Create(
-    const std::vector<ShaderPtr> &shaders)
+ProgramUPtr Program::Create(const std::vector<ShaderPtr> &shaders)
 {
     auto program = ProgramUPtr(new Program());
     if (!program->Link(shaders))
@@ -9,18 +8,22 @@ ProgramUPtr Program::Create(
     return std::move(program);
 }
 
-Program::~Program() {
-  if (m_program) {
-    glDeleteProgram(m_program);
-  }
+Program::~Program() 
+{
+    if (m_program) 
+    {
+        glDeleteProgram(m_program);
+    }
 }
 
-bool Program::Link(
-    const std::vector<ShaderPtr> &shaders)
+bool Program::Link(const std::vector<ShaderPtr> &shaders)
 {
     m_program = glCreateProgram();
     for (auto &shader : shaders)
+    {
         glAttachShader(m_program, shader->Get());
+    }
+
     glLinkProgram(m_program);
     int success = 0;
     glGetProgramiv(m_program, GL_LINK_STATUS, &success);
@@ -34,6 +37,19 @@ bool Program::Link(
     return true;
 }
 
-void Program::Use() const {
+void Program::Use() const 
+{
     glUseProgram(m_program);
+}
+
+void Program::SetUniform(const std::string &name, int value) const
+{
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniform1i(loc, value);
+}
+
+void Program::SetUniform(const std::string &name, const glm::mat4 &value) const
+{
+    auto loc = glGetUniformLocation(m_program, name.c_str());
+    glUniformMatrix4fv(loc, 1, GL_FALSE, glm::value_ptr(value));
 }
